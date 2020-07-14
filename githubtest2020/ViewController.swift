a6cd0768877d95ad7fd2470f126dc6f4ed351d5a
Dami Awofisayo <damismle1@gmail.com>
2020-07-14 12:01:32 -0400

    Initial Commit

---
+++ githubtest2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubtest2020
+//
+//  Created by Damilola Awofisayo on 7/14/20.
+//  Copyright © 2020 Damilola Awofisayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
